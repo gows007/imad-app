a1c3a737485e362236c032dc5cdce9737b3a74a8
Gowrishankar <gowrishankar.murali@gmail.com>
2017-08-03 01:17:33 +0200

    Added styling in css Added html files and acess then via server.js Create html content in js file and acess its corres function when the html file called

--- server.js
+++ server.js
@@ -5,22 +5,79 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+
+var articleOne ={
+  title: 'Article One | Gowrishankar',
+  heading: 'Article-one',
+  date: 'Aug 2, 2017',
+  content:    `
+          <p>
+            This is the first content of Article.In the search bar type in the name of the package you are looking to download. I don’t like installing package this way because you ending have a list of packages that you are not really looking for.  I like going through the command line to install packages.
+          </p>
+          <p>
+            Whether you’re in high school or college, you spend a lot of time taking notess. You have several excellent options for doing so, and which works best for you depends a lot on your note taking style. It’s best to pick software and stick to it so you don’t to worry about moving stuff around later. Here’s how to make the right choice from the outset.
+          </p>
+          <p>
+            Standing 1 ft 4 in  tall, Pikachu were the first "Electric-type" Pokémon created, their design intended to revolve around the concept of electricity.[9] They appear as Pika-like creatures that have short, yellow fur with brown markings covering their backs and parts of their lightning bolt shaped tails. They have black-tipped, pointed ears and red circular pouches on their cheeks, which can spark with electricity.[10] In Pokémon Diamond and Pearl, gender differences were introduced; a female Pikachu now has an indent at the end of its tail, giving it a heart-shaped appearance. They attack primarily by projecting electricity from their bodies at their targets. Within the context of the franchise, a Pikachu can transform, or "evolve" into a Raichu when exposed to a "Thunderstone". In later titles an evolutionary predecessor was introduced named "Pichu", which evolves into a Pikachu after establishing a close friendship with its trainer.
+          </p>`
+}
+
+function createTemplate(data){
+  var title = data.title;
+  var heading = data.heading;
+  var date = data.date;
+  var content = data.content;
+  var htmlTemplate = `
+  <html>
+    <head>
+      <title>
+        ${title}
+      </title>
+      <!-- To enable view in Mobile browser -->
+      <meta name="viewport" content="width=device-width, initial-scale=1"/>
+      <link href="/ui/style.css" rel="stylesheet" />
+    </head>
+
+    <body>
+      <div class="container">
+        <div>
+          <a href="/">Home</a>
+        </div>
+        <hr/>
+        <h3>
+          ${heading}
+        </h3>
+        <div>
+          ${date}
+        </div>
+        <div>
+          ${content}
+        </div>
+      </div>
+    </body>
+
+
+  </html>
+
+  `;
+  return htmlTemplate
+}
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
 app.get('/article-one',function (req, res) {
-  res.send('Article one requested will be server here')
-
+  res.send(createTemplate(articleOne));
 });
 
 app.get('/article-two',function (req, res) {
-  res.send('Article two requested will be server here')
+  res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
 
 });
 
 app.get('/article-three',function (req, res) {
-  res.send('Article three requested will be server here')
+  res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
 
 });
 
