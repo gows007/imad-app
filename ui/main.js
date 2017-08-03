04896bec3343328a99c08759de23b659c0b94f72
Gowrishankar <gowrishankar.murali@gmail.com>
2017-08-03 02:11:48 +0200

    Change contents of html file via main.js, i.e. clientside JS
    implemented changes on click of an element

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,12 @@
 console.log('Loaded!');
+
+//Change the text of main-text div
+var element = document.getElementById('main-text');
+
+element.innerHTML = 'New Text';
+
+//move image
+var img = document.getElementById('madi');
+img.onclick = function(){
+  img.style.marginLeft = '100px';
+}
