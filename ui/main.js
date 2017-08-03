7f8dd806a517798b6b2cf8aff3454b89c405bd60
Gowrishankar <gowrishankar.murali@gmail.com>
2017-08-03 02:58:09 +0200

    Now adding a counter button: 	<button>
    Get a request into JS by XMLHttpRequest() functio

--- ui/main.js
+++ ui/main.js
@@ -1,22 +1,30 @@
 console.log('Loaded!');
 
-//Change the text of main-text div
-var element = document.getElementById('main-text');
 
-element.innerHTML = 'New Text';
+var button = document.getElementById('counter');
+var counter = 0;
+button.onclick = function(){
+  //Create a request to counter endpoint
+  var request = new XMLHttpRequest();
 
-//move image
-var img = document.getElementById('madi');
-var marginLeft = 0;
-function moveRight(){
-  marginLeft = marginLeft + 2;
-  if(marginLeft > 400){
-    marginLeft = 0;
-  }
-  img.style.marginLeft = marginLeft + 'px';
-}
+  //Capture the response and store it in a variable
+  request.onreadystatechange = function(){
+    if(request.readyState === XMLHttpRequest.DONE){
+      //Take some action
+      if(request.status === 200){ //request is sucessfull
+        var counter = request.responseText;
+        var span = document.getElementById('count');
+        span.innerHTML = counter.toString();
+      }
+    }
+    //Not done then do nothing
+  };
 
-img.onclick = function(){
-  //Apply moveRight function every 100 ms
-  var interval = setInterval(moveRight, 50);
+  //Make a request
+  request.open('GET','http://localhost/counter',true);
+  request.open('GET','http://gows007.imad.hasura-app.io/counter',true);
+
+  //Render the variable in the correct <span>
+  counter = counter+1;
+
 };
