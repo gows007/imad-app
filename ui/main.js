6228e3cd84a45d1cdaa63093db00bed042ba6d8b
Gowrishankar <gowrishankar.murali@gmail.com>
2017-08-03 02:20:00 +0200

    Implemented animation for image via setInterval function

--- ui/main.js
+++ ui/main.js
@@ -7,6 +7,16 @@ element.innerHTML = 'New Text';
 
 //move image
 var img = document.getElementById('madi');
-img.onclick = function(){
-  img.style.marginLeft = '100px';
+var marginLeft = 0;
+function moveRight(){
+  marginLeft = marginLeft + 2;
+  if(marginLeft > 400){
+    marginLeft = 0;
+  }
+  img.style.marginLeft = marginLeft + 'px';
 }
+
+img.onclick = function(){
+  //Apply moveRight function every 100 ms
+  var interval = setInterval(moveRight, 50);
+};
