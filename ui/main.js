e9f8ffcd0da046e59047a8416d94e6b50a54b22e
gows007 <gows007@users.noreply.github.com>
2017-08-26 00:30:12 +0200

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,35 +1,6 @@
 console.log('Loaded!');
 
 
-var button = document.getElementById('counter');
-var counter = 0;
-button.onclick = function(){
-  //Create a request to counter endpoint
-  var request = new XMLHttpRequest();
-  //Capture the response and store it in a variable
-  request.onreadystatechange = function(){
-    if(request.readyState === XMLHttpRequest.DONE){
-      //Take some action
-      if(request.status === 200){ //request is sucessfull
-        var counter = request.responseText;
-        var span = document.getElementById('count');
-        span.innerHTML = counter.toString();
-      }
-    }
-    //Not done then do nothing
-  };
-
-  //Make a request
-  //request.open('GET','http://gows007.imad.hasura-app.io/counter',true);
-  request.open('GET','http://localhost/counter',true);
-  request.send(null);
-
-
-  //Render the variable in the correct <span>
-  counter = counter+1;
-
-};
-
 //Submit Username
 
 var submit = document.getElementById('submit_btn');
