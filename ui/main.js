28d50459629ec5f5de38c614ec378214ac2be0be
gows007 <gows007@users.noreply.github.com>
2017-08-26 00:23:20 +0200

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -30,7 +30,7 @@ button.onclick = function(){
 
 };
 
-//Submit name
+//Submit Username
 
 var submit = document.getElementById('submit_btn');
 submit.onclick = function(){
@@ -41,24 +41,29 @@ submit.onclick = function(){
     if(request.readyState === XMLHttpRequest.DONE){
       //Take some action
       if(request.status === 200){ //request is sucessfull
-        var names = request.responseText;
-        names = JSON.parse(names);
-        var list = '';
-        for(var i=0; i<names.length; i++){
-          list += '<li>' + names[i] + '</li>';
-        }
-        var ul = document.getElementById('nameList');
-        ul.innerHTML = list;
-
+        console.log('user logged in');
+        alert('Logged in Sucessfull');
+      }else{
+          if(request.status === 403){
+              alert('username/password is incorrect');
+          }else{
+              if(request.status === 403){
+              alert('somewthing went wrong with server');
+              }
+          } 
       }
     }
   //Capture list of names and render it as a list
 };
 //request.open('GET','http://gows007.imad.hasura-app.io/submit-name?name='+name,true);
-var nameInput = document.getElementById('name');
-var name = nameInput.value;
-request.open('GET','http://localhost/submit-name?name='+name,true);
-request.send(null);
+var username = document.getElementById('username').value;
+var password = document.getElementById('password').value;
+console.log(username);
+console.log(password);
+
+request.open('POST','http://gows007.imad.hasura-app.io/login',true);
+request.setRequestHeader('Content-Type','application/json');
+request.send(JSON.stringify({username: username, password:password}));
 
 };
 
