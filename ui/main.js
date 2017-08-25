1702762f7346eb75ca9f8ef9743dd6e5ba30e6b8
gows007 <gows007@users.noreply.github.com>
2017-08-26 00:32:25 +0200

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -38,34 +38,3 @@ request.send(JSON.stringify({username: username, password:password}));
 
 };
 
-var submit_cmt = document.getElementById('submit_cmt');
-submit_cmt.onclick = function(){
-  //Make a request to server and send the name
-  var request = new XMLHttpRequest();
-  //Capture the response and store it in a variable
-  request.onreadystatechange = function(){
-    if(request.readyState === XMLHttpRequest.DONE){
-      //Take some action
-      if(request.status === 200){ //request is sucessfull
-        var comments = request.responseText;
-        comments = JSON.parse(comments);
-        var comment_list = '';
-        for(var i=0; i<comments.length; i++){
-          comment_list += '<li>' + comments[i] + '</li>';
-        }
-        var ul = document.getElementById('commentList');
-        ul.innerHTML = comment_list;
-
-      }
-    }
-  //Capture list of comment and render it as a list
-};
-
-var commentInput = document.getElementById('comment');
-var comment = commentInput.value;
-var nameInput = document.getElementById('name');
-var name = nameInput.value;
-request.open('GET','http://localhost/submit-comment?comment='+comment+'&name='+name,true);
-request.send(null);
-
-};
