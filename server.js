6dbb57d1dff05bdc63c19184724cb7cdb4910760
gows007 <gows007@users.noreply.github.com>
2017-08-26 14:54:52 +0200

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -136,8 +136,9 @@ app.post('/login', function(req,res){
                   var salt = dbString.split('$')[2];
                   var hashedPassword = hash(password,salt);
                   if(hashedPassword == dbString){
-                      res.send('credentials correct!');
                       req.session.auth = {userId: result.rows[0].id};
+                      res.send('credentials correct!');
+                      
 
                       //Set a session available as a lib ans use cookies
 
