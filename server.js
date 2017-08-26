9c45708efaedf04ec321fd9719309cddae7b4682
gows007 <gows007@users.noreply.github.com>
2017-08-26 02:09:27 +0200

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -26,11 +26,10 @@ var pool = new Pool(config);
 
 
 var app = express();
-/*
+
 app.use(session({
     secret: 'randomSecretText',
-    cookie: { maxAge: 1000*60*60*24}
-}));*/
+}));
 app.use(morgan('combined'));
 app.use(bodyParser.json());
 
