02739680897e74c3279477840bf7fa8cd0f48aa1
gows007 <gows007@users.noreply.github.com>
2017-08-26 14:31:17 +0200

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -32,8 +32,8 @@ var username = document.getElementById('username').value;
 var password = document.getElementById('password').value;
 console.log(username);
 console.log(password);
-//request.open('POST','http://gows007.imad.hasura-app.io/login',true);
-request.open('POST','http://localhost/login',true);
+request.open('POST','http://gows007.imad.hasura-app.io/login',true);
+//request.open('POST','http://localhost/login',true);
 request.setRequestHeader('Content-Type','application/json');
 request.send(JSON.stringify({username: username, password:password}));
 
