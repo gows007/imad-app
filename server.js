117558ce3c02d79b96de4f39b97a8330bcd28469
gows007 <gows007@users.noreply.github.com>
2017-08-26 02:04:54 +0200

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -26,15 +26,12 @@ var pool = new Pool(config);
 
 
 var app = express();
-app.use(morgan('combined'));
-app.use(bodyParser.json());
-app.set('trust proxy',1);// trust first proxy 
 app.use(session({
     secret: 'randomSecretText',
     cookie: { maxAge: 1000*60*60*24}
 }));
-
-
+app.use(morgan('combined'));
+app.use(bodyParser.json());
 
 function createTemplate(data){
   var title = data.title;
